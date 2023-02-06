ff2f3d8ea2b98baeeb5ba33b09c9c059185fe959
CalebRose <ctrose17@gmail.com>
2023-02-05 17:56:19 -0800

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PlusIcon } from "./PlusIcon";
+import { MinusIcon } from "./MinusIcon";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {}, [count]);
+
+  const Reset = () => setCount(() => 0);
+
+  const Minus = () => {
+    if (count === 0) return;
+    setCount(() => count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-sm">
+      <div className="row justify-content-center d-flex">
+        <div className="mt-3">
+          <h1 className="title">Cookie's Crochet Counts</h1>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-4">
+            <button type="button" className="btn btn-dark" onClick={Reset}>
+              <h4>Reset Count</h4>
+            </button>
+          </div>
+          <div className="col-md-4" />
+          <div className="col-md-4"></div>
+        </div>
+        <div className="row vertical-align">
+          <div className="col count">{count}</div>
+        </div>
+        <div className="row">
+          <div className="col mt-3">
+            <button
+              type="button"
+              className="btn btn-white"
+              onClick={() => setCount(() => count + 1)}
+            >
+              <PlusIcon height={128} width={128} />
+            </button>
+          </div>
+          <div className="col mt-3">
+            <button type="button" className="btn btn-white" onClick={Minus}>
+              <MinusIcon height={128} width={128} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
